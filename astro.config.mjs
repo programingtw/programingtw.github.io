803ccd5fa6cea2bafeecb463de81621d9e58e743
jimmyhealer <yahing6066@gmail.com>
2024-07-24 15:37:46 +0800

    chore: add github page config

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,6 +25,7 @@ const whenExternalScripts = (items = []) =>
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
+  site: 'https://programingtw.github.io',
   output: 'static',
 
   integrations: [
